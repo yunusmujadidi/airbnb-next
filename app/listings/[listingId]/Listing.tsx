9805914d67f2bd65c10af3b96a093bcd36819657
Ahmad Yunus <yunusmujadidi@gmail.com>
2023-12-07 23:25:18 +0700

    create listing id page

--- app/listings/[listingId]/Listing.tsx
+++ app/listings/[listingId]/Listing.tsx
@@ -1,6 +1,9 @@
+import Container from "@/app/components/Container";
+import { categories } from "@/app/components/navbar/Categories";
 import { SafeListing, SafeUser } from "@/app/types";
 import { Reservation } from "@prisma/client";
 import React from "react";
+import ListingHead from "./ListingHead";
 
 interface ListingProps {
   reservation?: Reservation[];
@@ -9,6 +12,25 @@ interface ListingProps {
   };
   currentUser?: SafeUser | null;
 }
-const Listing = ({ listing, currentUser }: ListingProps) => {};
+const Listing = ({ listing, currentUser }: ListingProps) => {
+  const category = () => {
+    return categories.find((item) => item.label === listing.category);
+  };
 
+  return (
+    <Container>
+      <div className="max-w-screen-lg mx-auto">
+        <div className="flex flex-col gap-6">
+          <ListingHead
+            title={listing.title}
+            imageSrc={listing.imageSrc}
+            locationValue={listing.locationValue}
+            id={listing.id}
+            currentUser={currentUser}
+          />
+        </div>
+      </div>
+    </Container>
+  );
+};
 export default Listing;
